5f75ca2249de9bd79bb40b5eac584ccb7dee301a
Mickael Grzelka <grzelka.mickael@gmail.com>
2019-09-19 17:14:20 +0200

    add variable

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const button = document.getElementById('enter');
+const input = document.getElementById('userinput');
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+
