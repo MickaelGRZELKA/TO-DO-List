6703d19ac05cdd4718f59328f78026aa6df68533
Mickael Grzelka <grzelka.mickael@gmail.com>
2019-09-22 16:50:25 +0200

    function style

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
+`use strict`;
 
-const button = document.getElementById('enter');
-const input = document.getElementById('userinput');
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+let li;
 
+styl();
 
-function inputlength(){
-    return input.value.length();
+function styl(){
+document.body.style.fontFamily = "Indie Flower";
 }
-
-function createListElement(){
-    li.appendChild(li.textContent(input.value));
-    ul.appendChild('li');
-    input.value = "";
-}
-
-function addByClick(){
-    if(input.length() > 0){
-        createListElement();
-    }
-}
-
-function addByKeypress(){
-    if(input.length() > 0 && event.keycode === 13){
-        createListElement();
-    }
-}
-
-button.addEventListener('click', addByClick);
-input.addEventListener('keypress', addByKeypress);
